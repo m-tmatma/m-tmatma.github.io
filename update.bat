09478ae91e08e1004f37d0ab978470d4901e16df
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-11-19 10:59:54 +0900

    サイトマップ更新

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 cd /d  %~dp0
 
-python update-h1-title.py
-python update-header-sitemap.py
-python update-sitemap.py
-python update-viewport.py
-python update-CRLF.py
+C:\Python27\python update-h1-title.py
+C:\Python27\python update-header-sitemap.py
+C:\Python27\python update-sitemap.py
+C:\Python27\python update-viewport.py
+C:\Python27\python update-CRLF.py
