e8431cfd22e0f6a2864f6e006547795ecc00a968
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-07-28 17:48:22 +0900

    配列を追加

--- python/script/test-ctypes-structure.py
+++ python/script/test-ctypes-structure.py
@@ -21,6 +21,15 @@ class TestBig(ctypes.BigEndianStructure):
         ("testb", ctypes.c_int),
         ]
 
+class TestBigArray(ctypes.BigEndianStructure):
+    _pack_ = 1
+    _fields_ = [
+        ("testc", ctypes.c_byte),
+        ("testa", ctypes.c_int),
+        ("testb", ctypes.c_int),
+        ("testd", ctypes.c_byte * 4),
+        ]
+
 def Test1():
     test1 = Test(1, 2, 3)
     print ("size = ", ctypes.sizeof(test1))
@@ -69,6 +78,27 @@ def Test3():
     print ("b = ", test2.testb)
     print ("c = ", test2.testc)
 
+def Test4():
+    test1 = TestBigArray(1, 2, 3)
+    print ("size = ", ctypes.sizeof(test1))
+    print ("a = ", test1.testa)
+    print ("b = ", test1.testb)
+    print ("c = ", test1.testc)
+
+    test1.testd[0] = 10
+    test1.testd[1] = 11
+    test1.testd[2] = 12
+    test1.testd[3] = 13
+    data = bytes(test1)
+
+    test2 = TestBigArray.from_buffer_copy(data)
+    print ("a = ", test2.testa)
+    print ("b = ", test2.testb)
+    print ("c = ", test2.testc)
+    for i, d in enumerate(test2.testd):
+        print (i, "d = ", d)
+
 Test1()
 Test2()
 Test3()
+Test4()
